ce13810a40d5522b07315da1bed49bb64bd3ce21
m-inata <m.inata303@gmail.com>
2021-11-09 15:27:29 +0900

    Add Kotlin file

---
+++ app/src/main/java/jp/m/inata/githubtest/Hello.kt
@@ -0,0 +1,5 @@
+package jp.m.inata.githubtest
+
+class Hello(val name: String) {
+    fun getMessage() = "こんにちは、" + name + "さん！"
+}
--- app/src/main/java/jp/m/inata/githubtest/MainActivity.java
+++ app/src/main/java/jp/m/inata/githubtest/MainActivity.java
@@ -3,12 +3,19 @@ package jp.m.inata.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view1);
+        Hello hello = new Hello("あああ");
+        textView.setText(hello.getMessage());
     }
 }
